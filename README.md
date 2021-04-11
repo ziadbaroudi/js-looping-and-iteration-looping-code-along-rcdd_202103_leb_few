0427abd078fc003d741a3e930a5928d03cd464da
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-11 18:21:02 +0000

    Done.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # Looping Lab
 
 ## Objectives
--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+// let a = ['Ziad','Robert','Jean']
+let message =[];
+function  writeCards(names,event){
+ for(let i = 0; i<names.length; i++){
+   message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+ } 
+ return message
+}
+ writeCards()
+
+let n = 10;
+//let i = 0;
+function countDown(n){
+  while(n>=0){
+    console.log(n);
+    n--;
+  }
+}
+countDown(n)
