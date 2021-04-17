b5880b82837c43b899bf81797db948c429a7e1d6
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-17 14:52:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solutions in this file
+<<<<<<< HEAD
 // let a = ['Ziad','Robert','Jean']
 let message =[];
 function  writeCards(names,event){
@@ -8,6 +9,8 @@ function  writeCards(names,event){
  return message
 }
  writeCards()
+=======
+>>>>>>> 78daeb5700532b4137d23e52a410985fcf67cb20
 
 let n = 10;
 //let i = 0;
@@ -18,3 +21,15 @@ function countDown(n){
   }
 }
 countDown(n)
+<<<<<<< HEAD
+=======
+let a = ['Ziad','Robert','Jean']
+
+funcion writeCards(a,e){
+ for(let i = 0; i<a.length; i++){
+   console.log(`Thank you, ${a[i]} fot the wonderful birthday gift!`)}
+ } 
+}
+
+writeCards(a,birthday)
+>>>>>>> 78daeb5700532b4137d23e52a410985fcf67cb20
